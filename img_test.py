59bd04b082d6ce6ae38265538461a35a8e192e6a
devOPStremblant <jaivijay.se@gmail.com>
2018-01-17 23:50:54 -0500

    changed looping

--- img_test.py
+++ img_test.py
@@ -12,3 +12,13 @@ M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
 dst = cv2.warpAffine(img,M,(cols,rows))
 
 plt.show()
+
+def decimal_range(start, stop, increment):
+    while start < stop: # and not math.isclose(start, stop): Py>3.5
+        yield start
+        start += increment
+Rangelow=0.11
+Rangehigh=1
+Delta=0.001
+for i in decimal_range(Rangelow, Rangehigh, Delta):
+	print i,Rangelow,Rangehigh,Delta
