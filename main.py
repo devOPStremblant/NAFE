be319093525ebec6a0807d494e17535a3b2a1ff7
Vijayakumar <VINODKUMAR_VIJAYAKUMAR@homedepot.com>
2018-03-08 16:52:34 -0500

    libtiff is installed, need to cross verify with Wah

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def process_image(im):
     for x in range (0, img_array.shape[0]):
         for y in range (0, img_array.shape[1]):
             # print "Working on (%s, %s)" % (x, y)
-            # filtered_pixel_value = filter_pixel_new(x, y, img_array)
-            # new_img[x][y]=filtered_pixel_value
+            filtered_pixel_value = filter_pixel_new(x, y, img_array)
+            new_img[x][y]=filtered_pixel_value
             new_img[x][y] = get_16bit_pixel_value(x, y, img_array)
 
     imsave('test_output.tif', data=(new_img), shape=(img_array.shape))
@@ -168,21 +168,24 @@ def filter_pixel_new(x, y, img_array):
     rho_index = lower_rho
     phi_index = lower_phi
 
+    loop_count = 0
     while rho_index < upper_rho:
         while phi_index < upper_phi:
             # print "Current Rho is %f and Current Phi is %f " % (rho_index, phi_index)
             neighbour_x, neighbour_y = polToCar(rho_index, phi_index)
-            neighbour_x += x0
-            neighbour_y = y0 - neighbour_y
+            neighbour_x = int(round(x0 + neighbour_x))
+            neighbour_y = int(round(y0 - neighbour_y))
             pv = get_16bit_pixel_value(int(round(neighbour_x)), int(round(neighbour_y)), img_array)
-            # print "f() function returns %s " % pv
+            print "Pixel Value of Neighboring pixel (%s, %s) is %s " % (neighbour_x, neighbour_y, pv)
             kernel_value = calculate_kernel(rho_index, phi_index, r, varphi)
-            # print "C() function of %s and %s is %s" % (rho_index, phi_index, kernel_value)
+            print "Kernel value with input parameters (%s, %s, %s, %s) is %s" % (rho_index, phi_index, r, varphi, kernel_value)
             sum_a += (pv * kernel_value)
             sum_b += kernel_value
-            phi_index += 0.1
-        rho_index += 0.1
+            phi_index += 1
+            loop_count += 1
+        rho_index += 1
 
+    print "Rho and Phi loops ran for %s times" % loop_count
     print "Sum Gaussian A: %s" % sum_a
     print "Sum Gaussian B: %s" % sum_b
 
@@ -436,12 +439,12 @@ o_img = read_image()
 # read_tiff_using_tifffle()
 
 # read_tiff_using_libtiff()
-# filter_pixel_new(20, 20, read_tiff_using_tifffle())
+filter_pixel_new(20, 20, read_tiff_using_tifffle())
 
 # write_img_with_polar_car(c_img, o_img)
 # write_img_test(c_img)
 
-process_image(o_img)
+# process_image(o_img)
 # calculate_degrees(32, -19, 3)
 # calculate_degrees(22, 24,1)
 # calculate_degrees(0,0, 4)
